6c934257677361516c6dcbfd2203b9c68e63a41b
uanand99 <uanand@phdata.io>
2024-04-05 18:51:55 +0530

    Initial commit

---
+++ scripts/flatfiles/V2.2.0__ff_table_2.sql
@@ -0,0 +1,6 @@
+USE SCHEMA SCH_EXTERNAL;
+
+CREATE OR REPLACE TABLE SRC_FF_2(
+    name text,
+    id number
+);
---
+++ scripts/oracle/V1.1.0__o_table_1.sql
@@ -0,0 +1,6 @@
+USE SCHEMA SCH_ORACLE;
+
+CREATE OR REPLACE TABLE SRC_O_1(
+    name text,
+    id number
+);
